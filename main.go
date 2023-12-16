681a0009c1272977fc1c73ff255fab49b2c9061b
khushiatgit <khushi.chauhan231@gmail.com>
2023-12-17 02:21:24 +0530

    Added the endpoint for reading the details of existing users in the system.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gofr.dev/pkg/gofr"
@@ -76,10 +78,73 @@ func CreateUserEndpoint(ctx *gofr.Context) (interface{}, error) {
 	return result, nil
 }
 
+// Get user details
+func GetUserEndpoint(ctx *gofr.Context) (interface{}, error) {
+	// Get user ID from path parameter
+	id := ctx.PathParam("id")
+
+	// Get database collection
+	collection := client.Database("UMS").Collection("users")
+
+	// If an ID is provided, retrieve a single user by ID
+	if id != "" {
+		// Convert the string ID to ObjectId
+		objectID, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			response := "Invalid ID format"
+			return response, err
+		}
+
+		// Find user by ID
+		var user User
+		err = collection.FindOne(context.TODO(), bson.M{"_id": objectID}).Decode(&user)
+		if err != nil {
+			response := "Could not fetch user details"
+			return response, err
+		}
+
+		// Return the user details
+		return user, nil
+	}
+
+	// If no ID is provided, retrieve all users
+	var users []User
+
+	// Find all users in the collection
+	cursor, err := collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		response := "Error fetching users"
+		return response, err
+	}
+	defer cursor.Close(context.TODO())
+
+	// Iterate over the cursor and decode each document into the users slice
+	for cursor.Next(context.TODO()) {
+		var user User
+		err := cursor.Decode(&user)
+		if err != nil {
+			response := "Error decoding user data"
+			return response, err
+		}
+		users = append(users, user)
+	}
+
+	// Check for errors from iterating over the cursor
+	if err := cursor.Err(); err != nil {
+		response := "Error iterating over users"
+		return response, err
+	}
+
+	// Return the list of users
+	return users, nil
+}
+
 func main() {
 	app := gofr.New()
 
 	app.POST("/users", CreateUserEndpoint)
+	app.GET("/users", GetUserEndpoint)
+	app.GET("/users/{id}", GetUserEndpoint)
 
 	app.Start()
 
